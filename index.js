c8f10c50e0c992b2bcba3f758fc9fa5ae5c216fe
Gabriel <gabrielulu@gmail.com>
2017-12-12 18:45:06 +0800

    testing

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const exphbs = require('express-handlebars')
+const path = require('path')
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', function (req, res) {
+  res.render('home')
+})
+
+app.listen(port, () => {
+  console.log(`App listening on server ${port}`)
+})
